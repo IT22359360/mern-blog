0040b73d32d94e99834d40fbc55ffe0b43c1dbe4
IT22359360 <it22359360@my.sliit.lk>
2024-03-12 20:09:53 +0530

    create pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
-import React from 'react';
+import React from 'react'
 
-const App = () => {
+export default function App() {
   return (
     <div>
-      <h1 className='text-3xl text-red-500'>App</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+
+      </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
